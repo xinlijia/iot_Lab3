4cd10fcbd4ea3515ba7afcbdc82832a889377b1b
gooliath12 <jamesguo1112@sina.com>
2017-02-15 14:34:45 -0500

    Minor changes.

--- utils/mtaUpdates.py
+++ utils/mtaUpdates.py
@@ -17,8 +17,10 @@ class mtaUpdates(object):
     # here we are using feed 1 , which has lines 1,2,3,4,5,6,S
     # While initializing we can read the API Key and add it to the url
     feedurl = 'http://datamine.mta.info/mta_esi.php?feed_id=1&key='
-    with open ("api_key.txt", "r") as apikey_file:
-        apikey = apikey_file.readlines()
+    # Read MTA API key from local .txt file  -- pg
+    with open ("api_key.txt", "rb") as apikey_file:
+        apikey = apikey_file.read().rstrip('\n')
+        apikey_file.close()
 
     VCS = {1:"INCOMING_AT", 2:"STOPPED_AT", 3:"IN_TRANSIT_TO"}
     tripUpdates = []
@@ -34,29 +36,28 @@ class mtaUpdates(object):
             with contextlib.closing(urllib2.urlopen(self.feedurl)) as response:
                 d = feed.ParseFromString(response.read())
         except (urllib2.URLError, google.protobuf.message.DecodeError) as e:
-            print "Error while connecting to mta server " +str(e)
+            print "Error while connecting to mta server: " +str(e)
 
-
-	timestamp = feed.header.timestamp
+	    timestamp = feed.header.timestamp
         nytime = datetime.fromtimestamp(timestamp,self.TIMEZONE)
 
-	for entity in feed.entity:
-	    # Trip update represents a change in timetable
-	    if entity.trip_update and entity.trip_update.trip.trip_id:
-		update = tripupdate.tripupdate()
+	    for entity in feed.entity:
+	        # Trip update represents a change in timetable
+	        if entity.trip_update and entity.trip_update.trip.trip_id:
+		        update = tripupdate.tripupdate()
+		        ##### INSERT TRIPUPDATE CODE HERE ####
 
-		##### INSERT TRIPUPDATE CODE HERE ####
 
-	    if entity.vehicle and entity.vehicle.trip.trip_id:
-	    	v = vehicle.vehicle()
 
-		##### INSERT VEHICLE CODE HERE #####
 
-	    if entity.alert:
-                a = alert.alert()=
+	        if entity.vehicle and entity.vehicle.trip.trip_id:
+	    	    v = vehicle.vehicle()
+		        ##### INSERT VEHICLE CODE HERE #####
 
+	        if entity.alert:
+                a = alert.alert()
                 #### INSERT ALERT CODE HERE #####
 
-	return self.tripUpdates
+	    return self.tripUpdates
 
-    # END OF getTripUpdates method
+        # END OF getTripUpdates method
