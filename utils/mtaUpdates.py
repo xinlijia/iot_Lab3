10a53f32554da56ebd6f9d65d02f50a0c426d271
gooliath12 <jamesguo1112@sina.com>
2017-02-15 10:21:13 -0500

    Setup MTA API key and proto buffers for assignment 1.

--- utils/mtaUpdates.py
+++ utils/mtaUpdates.py
@@ -12,13 +12,15 @@ class mtaUpdates(object):
 
     # Do not change Timezone
     TIMEZONE = timezone('America/New_York')
-    
+
     # feed url depends on the routes to which you want updates
     # here we are using feed 1 , which has lines 1,2,3,4,5,6,S
     # While initializing we can read the API Key and add it to the url
     feedurl = 'http://datamine.mta.info/mta_esi.php?feed_id=1&key='
-    
-    VCS = {1:"INCOMING_AT", 2:"STOPPED_AT", 3:"IN_TRANSIT_TO"}    
+    with open ("api_key.txt", "r") as apikey_file:
+        apikey = apikey_file.readlines()
+
+    VCS = {1:"INCOMING_AT", 2:"STOPPED_AT", 3:"IN_TRANSIT_TO"}
     tripUpdates = []
     alerts = []
 
@@ -33,28 +35,28 @@ class mtaUpdates(object):
                 d = feed.ParseFromString(response.read())
         except (urllib2.URLError, google.protobuf.message.DecodeError) as e:
             print "Error while connecting to mta server " +str(e)
-	
+
 
 	timestamp = feed.header.timestamp
         nytime = datetime.fromtimestamp(timestamp,self.TIMEZONE)
-	
+
 	for entity in feed.entity:
 	    # Trip update represents a change in timetable
 	    if entity.trip_update and entity.trip_update.trip.trip_id:
 		update = tripupdate.tripupdate()
 
-		##### INSERT TRIPUPDATE CODE HERE ####	
+		##### INSERT TRIPUPDATE CODE HERE ####
 
 	    if entity.vehicle and entity.vehicle.trip.trip_id:
 	    	v = vehicle.vehicle()
 
 		##### INSERT VEHICLE CODE HERE #####
-	    
+
 	    if entity.alert:
                 a = alert.alert()=
 
                 #### INSERT ALERT CODE HERE #####
 
 	return self.tripUpdates
-    
+
     # END OF getTripUpdates method
