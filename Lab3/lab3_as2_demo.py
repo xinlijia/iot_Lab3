9ceab56bfaf4e955000f4219cc331c5fd014a1a1
gooliath12 <jamesguo1112@sina.com>
2017-02-18 13:50:45 -0500

    处理了创建已存在table的异常情况；使用event，实现了在主线程通过keyboardInterrput控制两个分线程的退出

--- Lab3/lab3_as2_demo.py
+++ Lab3/lab3_as2_demo.py
@@ -5,6 +5,8 @@ import boto
 import boto.dynamodb2
 from pytz import timezone
 import calendar
+import logging
+
 
 ### Connect to AWS & Set a new table in Dynamo
 # Change 'aws_connect.txt' below to your file.
@@ -30,66 +32,78 @@ client_dynamo = boto.dynamodb2.connect_to_region(
     aws_secret_access_key=assumedRoleObject.credentials.secret_key,
     security_token=assumedRoleObject.credentials.session_token)
 
-# Create New Table. ID as hashkey; Time as RangeKey.
+# Create New Table or use existing table.
+# ID as hashkey; Time as RangeKey.
 from boto.dynamodb2.table import Table
 from boto.dynamodb2.fields import HashKey, RangeKey
 DYNAMODB_TABLE_NAME = 'add_clean_test'
-# ac = Table.create(DYNAMODB_TABLE_NAME, schema=[HashKey('ID'), RangeKey('Time')]);
-# Use Existing Table.
-ac = Table(DYNAMODB_TABLE_NAME)
+# Handle error condition: Creating a table that already exists.
+try:
+    # Try creating a new table.
+    ac = Table.create(DYNAMODB_TABLE_NAME, schema=[HashKey('ID'), RangeKey('Time')])
+except boto.exception.JSONResponseError:
+    # Use existing table.
+    ac = Table(DYNAMODB_TABLE_NAME)
+
 
 ### Two functions: adding and cleaning
-def adding(table):
-    ctr = 0
-    while(True):
-        try:
-            # every 2.5 seconds, add 5 tuples to table
-            for i in range(5):
-                ctr += 1
-                # Get current time in POSIX format & datetime (human-readable) format.
-                d = datetime.utcnow()
-                cur_time = calendar.timegm(d.utctimetuple())
-                cur_time_readable = datetime.fromtimestamp(cur_time, timezone('America/New_York'))
-                # Write new data to Dynamo
-                table.put_item(
-                    data={
-                        'ID': str(ctr),
-                        'Time': str(cur_time),
-                        'Time(Readable)':str(cur_time_readable)})
-            time.sleep(2.5)
-        # Keyboard Interrupt to terminate the
-        except KeyboardInterrupt:
-            print "QUIT adding process."
-            break
-        finally:
-            pass
-    return None
+# Configure logging: "(threadName) message" format
+logging.basicConfig(level=logging.DEBUG,
+                    format='(%(threadName)-8s) %(message)s',
+                    )
 
-def cleaning(table):
-    while(True):
-        try:
-            # every 5 seconds, delete 10s'old tuples
-            # Get current time.
+def adding(table, run_event):
+    logging.debug('Starting.')
+    ctr = 0
+    # every 2.5 seconds, add 5 tuples to table
+    while(run_event.is_set()):
+        for i in range(5):
+            ctr += 1
+            # Get current time in POSIX format & datetime (human-readable) format.
             d = datetime.utcnow()
             cur_time = calendar.timegm(d.utctimetuple())
-            # Read all data in table.
-            all_data = table.scan()
-            # Find all old data and delete them.
-            for data in all_data:
-                if cur_time - int(data['Time']) > 10:
-                    data.delete()
-            time.sleep(5)
-        except keyboardInterrupt:
-            print "QUIT cleaning process."
-            break
-        finally:
-            pass
+            cur_time_readable = datetime.fromtimestamp(cur_time, timezone('America/New_York'))
+            # Write new data to Dynamo
+            table.put_item(
+                data={
+                    'ID': str(ctr),
+                    'Time': str(cur_time),
+                    'Time(Readable)':str(cur_time_readable)})
+        time.sleep(2.5)
+
+    # run_event is cleared (run_event.is_set() == False), exit the thread
+    logging.debug('Exiting.')
 
     return None
 
-### Two Threads: adding and cleaning
-t1 = threading.Thread(target = adding, name = 'adding', args=(ac,))    # ac: table in DynamoDB
-t2 = threading.Thread(target = cleaning, name = 'cleaning', args=(ac,))
+def cleaning(table, run_event):
+    logging.debug('Starting.')
+    # every 5 seconds, delete all 10-second-old data
+    while(run_event.is_set()):
+        # Get current time.
+        d = datetime.utcnow()
+        cur_time = calendar.timegm(d.utctimetuple())
+        # Read all data in table.
+        all_data = table.scan()
+        # Find all old data and delete them.
+        for data in all_data:
+            if cur_time - int(data['Time']) > 10:
+                data.delete()
+
+    # run_event.is_set() == False, exiting the thread.
+    logging.debug('Exiting.')
+
+    return None
+
+
+### MAIN PROGRAM ###
+# Set an event for sending the exit signal to threads t1 & t2
+run_event = threading.Event()
+run_event.set()
+
+# Two Threads: adding and cleaning
+t1 = threading.Thread(target = adding, name = 'adding', args=(ac, run_event))    # ac: table in DynamoDB
+t2 = threading.Thread(target = cleaning, name = 'cleaning', args=(ac, run_event))
 
 # Set to Daemon mode
 t1.setDaemon(True)
@@ -98,6 +112,14 @@ t2.setDaemon(True)
 t1.start()
 t2.start()
 
-# Wait for threads t1 & t2 to finish
-t1.join()
-t2.join()
+try:
+    while True:
+        time.sleep(0.1)
+except keyboardInterrupt:
+    print "Keyboard Interrupt, attempting to close the threads."
+    # Send exit signals.
+    run_event.clear()
+    # Wait for t1 & t2 to exit.
+    t1.join()
+    t2.join()
+    print "Threads successfully closed."
