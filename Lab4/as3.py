fa7c411e8258dcd8e85adabf7a34efaba4ab0d45
gooliath12 <jamesguo1112@sina.com>
2017-02-28 03:06:13 -0500

    修复了SMS中连接AWS的问题

--- Lab4/as3.py
+++ Lab4/as3.py
@@ -146,10 +146,17 @@ def main():
 
 # send subscribe message to the given phone number
 def sendSubMes(result):
+    # Read key fiies
+    with open('../utils/sns_key.txt', 'rb') as aws_file:
+    	lines = aws_file.readlines()
+    	access_key_id = lines[0].rstrip('\n')
+    	secret_access_key = lines[1].rstrip('\n')
 
+    # Connect to Amazon AWS
     conn = boto.sns.connect_to_region("us-east-1",
-                aws_access_key_id = '',
-                aws_secret_access_key = '')
+    		aws_access_key_id = access_key_id,
+    		aws_secret_access_key = secret_access_key)
+            
     TOPIC = 'arn:aws:sns:us-east-1:936464516303:Demo_Topic'
     add_sub = raw_input("Please input your phone number : ")
     conn.subscribe(TOPIC,"SMS",add_sub)
