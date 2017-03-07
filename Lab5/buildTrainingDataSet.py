459970e48a9c6ec30a6a755a5ea2f8ba2138765d
gooliath12 <jamesguo1112@sina.com>
2017-03-07 02:33:19 -0500

    实现功能

--- Lab5/buildTrainingDataSet.py
+++ Lab5/buildTrainingDataSet.py
@@ -13,10 +13,19 @@ from pytz import timezone
 from datetime import datetime
 
 
-TIMEZONE = timezone('America/New_York')
+def pts(time):
+    """
+	pts: POSIX TO SECONDS
+    Convert POSIX time format to human-readable format.
+    "YYYY-MM-DD HH:MM:SS"
+    """
+
+    hms = str(datetime.fromtimestamp(time, timezone('America/New_York')))[11:19]
+    seconds = int(hms.split(':')[0]) * 3600 + int(hms.split(':')[1]) * 60 + int(hms.split(':')[2])
+    return seconds
 
 
-def main(fileHandle):
+def gencdv(fileHandle, w_a, c_r):
 	# This creates a dataframe
 	rawData = read_csv(fileHandle)
 
@@ -24,14 +33,14 @@ def main(fileHandle):
 	data = rawData.groupby('tripId').apply(lambda x: x.ix[x.timestamp.idxmax()])
 
 	# Seperate all the local trains and form a new data frame
-	localTrains = data[data.route == '1']
+	localTrains = data[data.route == 1]
 
 	# Express trains
 	#expressTrains = data[data.route == 'E']
 
     # Line 2 & Line 3 trains
-    line2Trains = data[data.route == '2']
-    line3Trains = data[data.route == '3']
+	line2Trains = data[data.route == 2]
+	line3Trains = data[data.route == 3]
 
 	# 1. Find the time difference (to reach 96th) between all combinations of local trains and express #####
 	# 2. Consider only positive delta
@@ -39,55 +48,94 @@ def main(fileHandle):
     # 4. Upload the final table to Amazon S3
 
 	# Create a new data frame for final table
-	finalData = pandas.DataFrame(columns=('arrive116Time', 'Wkdy/Wknd', 'Recommendation'))
+	#### CLASSIFICATION
+	if c_r == 'c':
+		finalData = pandas.DataFrame(columns=('timestamp','Wkdy/Wknd','arrive116Time','arrive96Time','arrive42Time','Recommendation'))
+	#### REGRESSION
+	else:
+		finalData = pandas.DataFrame(columns=('Wkdy/Wknd','arrive116Time','Line1Cost','Line2Cost','Line3Cost'))
 
 	############## INSERT YOUR CODE HERE ###############
-    idx = 0  # row index for finalData
+	idx = 0  # row index for finalData
 
     # Check data's validity, remove invalid rows
-    localTrains = localTrains[lcoalTrains.arrive116Time and localTrains.arrive96Time and localTrains.arrive42Time]
-    localTrains = localTrains[lcoalTrains.arrive116Time < localTrains.arrive96Time < localTrains.arrive42Time]
+	localTrains = localTrains[localTrains.timeToReachSource < localTrains.timeToReachExpressStation]
+	localTrains = localTrains[localTrains.timeToReachExpressStation < localTrains.timeToReachDestination]
+	line2Trains = line2Trains[line2Trains.timeToReachExpressStation < line2Trains.timeToReachDestination]
+	line3Trains = line3Trains[line3Trains.timeToReachExpressStation < line3Trains.timeToReachDestination]
 
-    line2Trains = line2Trains[line2Trains.arrive96Time and line2Trains.arrive42Time]
-    line2Trains = line2Trains[line2Trains.arrive96Time < line2Trains.arrive42Time]
+	# For Each Line 1 train arriving 116th Street,
+	# compute whether "SWITCH" or "STAY" at 96th Street
+	for index, line1Row in localTrains.iterrows():
+		# Extract each entry's attributes in data.csv
+		timestamp = pts(line1Row['timestamp'])
+		# Weekday --> 1; Weekend --> 0
+		if line1Row['day'] == 'Weekday':
+			wdwe = 1
+		else:
+			wdwe = 0
 
-    line3Trains = line3Trains[line3Trains.arrive96Time and line3Trains.arrive42Time]
-    line3Trains = line3Trains[line3Trains.arrive96Time < line3Trains.arrive42Time]
+		line1Arrive116Time = pts(line1Row['timeToReachSource'])
+		line1Arrive96Time = pts(line1Row['timeToReachExpressStation'])
+		line1Arrive42Time = pts(line1Row['timeToReachDestination'])
 
-    # For Each Line 1 train arriving 116th Street,
-    # compute whether "SWITCH" or "STAY" at 96th Street
-    for index, line1Row in localTrains.iterrows():
-        line1Arrive116Time = line1Row['arrive116Time']
-        line1Arrive96Time = line1Row['arrive96Time']
-        line1Arrive42Time = line1Row['arrive42Time']
+		# Time duration
+		line1Td = line1Arrive42Time - line1Arrive116Time
+		try:
+			line2Available = line2Trains[line2Trains.timeToReachExpressStation > line1Row['timeToReachExpressStation']]
+			line2Arrive42Time = pts(line2Available['timeToReachDestination'].min())
+			line2Td = line2Arrive42Time - line1Arrive116Time
+		# There could be cases where line 1 train cannot find it's matching Line 2 train
+		except (IndexError, ValueError) as e:
+			continue
+		    # line2Arrive42Time = 9999999999 # No applicable Line 2 Trains
 
-        try:
-            line2Available = line2Trains[line2Trains.arrive96Time > line1Arrive96Time]
-            line2Arrive42Time = line2Available['arrive42Time'].min()
-        # There could be cases where line 1 train cannot find it's matching Line 2 train
-        except IndexError:
-            line2Arrive42Time = 9999999999 # No applicable Line 2 Trains
+		try:
+			line3Available = line3Trains[line3Trains.timeToReachExpressStation > line1Row['timeToReachExpressStation']]
+			line3Arrive42Time = pts(line3Available['timeToReachDestination'].min())
+			line3Td = line3Arrive42Time - line1Arrive116Time
+		except (IndexError, ValueError) as e:
+			continue
+		    # line3Arrive42Time = 9999999999 # No applicable Line 3 Trains
 
-        try:
-            line3Available = line3Trains[line3Trains.arrive96Time > line1Arrive96Time]
-            lin3Arrive42Time = line3Available['arrive42Time'].min()
-        except IndexError:
-            line3Arrive42Time = 9999999999 # No applicable Line 3 Trains
+		#### CLASSIFICATION
+		# Based on "delta", choose whether to 'SWITCH' or 'STAY'
+		if line1Arrive42Time <= line2Arrive42Time and line1Arrive42Time <= line3Arrive42Time:
+		    recom = 1  # Stay on line 1
+		elif line2Arrive42Time <= line1Arrive42Time and line2Arrive42Time <= line3Arrive42Time:
+		    recom = 2  # Switch to line 2
+		else:
+		    recom = 3  # Switch to line 3
 
-        # Based on "delta", choose whether to 'SWITCH' or 'STAY'
-        if line1Arrive42Time <= line2Arrive42Time and line1Arrive42Time <= line3Arrive42Time:
-            recom = 'STAY'
-        elif line2Arrive42Time <= line1Arrive42Time and line2Arrive42Time <= line3Arrive42Time:
-            recom = 'SWITCH TO LINE 2'
-        else:
-            recom = 'SWITCH TO LINE 3'
+		if recom == 2 or recom == 3:
+			recom = 0  # Switch
 
-        finalData.loc[idx] = [line1Arrive116Time, line1Row['dow'], recom]
-        idx += 1
+		#### CLASSIFICATION
+		if c_r == 'c':
+			## columns=('timestamp','Wkdy/Wknd', 'arrive116Time', 'arrive96Time', 'arrive42Time','Recommendation')
+			finalData.loc[idx] = [timestamp, wdwe, line1Arrive116Time, line1Arrive96Time, line1Arrive42Time, recom]
+		#### REGRESSION
+		else:
+			## columns=('Wkdy/Wknd','arrive116Time','Line1Cost','Line2Cost','Line3Cost')
+			finalData.loc[idx] = [wdwe, line1Arrive116Time, line1Td, line2Td, line3Td]
 
-    ############## END OF CODE ####################
+		idx += 1
 
-	finalData.to_csv("finalData.csv",index=False)
+	############## END OF CODE ####################
+	# Write new .csv file or append existing file
+	if w_a == 'a':
+		finalData.to_csv("finalData.csv", mode='ab', header=False, index=False)
+	else:
+		finalData.to_csv("finalData.csv", mode='w', index=False)
+
+
+def main(fh1, fh2, fh3, fh4, fh5, c_r):
+	fh_list = [fh1, fh2, fh3, fh4, fh5]
+	for fh in fh_list:
+		if fh == fh_list[0]:
+			gencdv(fh, w_a='w', c_r=c_r)
+		else:
+			gencdv(fh, w_a='a', c_r=c_r)
 
 
 
@@ -96,13 +144,18 @@ if __name__ == "__main__":
 	lengthArg = len(sys.argv)
 
 
-	if lengthArg < 2:
+	if lengthArg < 7:
 		print "Missing arguments"
 		sys.exit(-1)
 
-	if lengthArg > 2:
+	if lengthArg > 7:
 		print "Extra arguments"
 		sys.exit(-1)
 
-	fileHandle = sys.argv[1]
-	main(fileHandle)
+	fileHandle1 = sys.argv[1]
+	fileHandle2 = sys.argv[2]
+	fileHandle3 = sys.argv[3]
+	fileHandle4 = sys.argv[4]
+	fileHandle5 = sys.argv[5]
+	c_r = sys.argv[6]
+	main(fileHandle1, fileHandle2, fileHandle3, fileHandle4, fileHandle5, c_r)
