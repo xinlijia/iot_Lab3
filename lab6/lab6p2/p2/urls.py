63a0e321108f32fcc1426045e44b2ee7b70a2daf
xinlijia <jiaxinli93@gmail.com>
2017-03-20 22:50:48 -0400

    Add files via upload
    add map function

--- lab6/lab6p2/p2/urls.py
+++ lab6/lab6p2/p2/urls.py
@@ -4,12 +4,12 @@ from . import views
 
 app_name = 'p2'
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.hp, name='hp'),
     #url(r'(?P<city_id>[0-9]+)/$', views.detail, name='detail'),
     #url(r'(?P<city_id>[0-9]+)/weather/$', views.weather, name='weather'),
-    url(r'app_name/(?P<src_id>[0-9]+)/(?P<des_id>[0-9]+)/$', views.results, name='results'),
+    url(r'(?P<src_id>[0-9]+)/(?P<des_id>[0-9]+)/$', views.index, name='index')
     #url(r'(?P<trip_id>[0-9]+)/results/$', views.results, name='results'),
-    
+
 
 
 ]
--- lab6/lab6p2/p2/views.py
+++ lab6/lab6p2/p2/views.py
@@ -6,31 +6,53 @@ import requests
 
 from .models import City, Trip
 
-def index(request):
-    city_list = City.objects.order_by('city_name')[:5]
-    context = {'city_list': city_list}
-    for city in city_list:
-        if(city.city_name=="New York"):
-            r = requests.get('http://api.openweathermap.org/data/2.5/weather?q=New%20York&APPID=bb740b159467f35d8fe7b27bb6bcf553')
-        elif(city.city_name=="Boston"):
-            r = requests.get('http://api.openweathermap.org/data/2.5/weather?q=Boston&APPID=bb740b159467f35d8fe7b27bb6bcf553')
-        elif(city.city_name=="Philadelphia"):
-            r = requests.get('http://api.openweathermap.org/data/2.5/weather?q=Philadelphia&APPID=bb740b159467f35d8fe7b27bb6bcf553')
-        else:
-            r = requests.get('http://api.openweathermap.org/data/2.5/weather?q=Washington&APPID=bb740b159467f35d8fe7b27bb6bcf553')
-        
-        city.temp=(r.json()['main']['temp'])
+def index(request, src_id, des_id):
+	city_list = City.objects.order_by('city_name')[:5]
+	# context = {'city_list': city_list}
+	for city in city_list:
+		if(city.city_name=="New York"):
+			r = requests.get('http://api.openweathermap.org/data/2.5/weather?q=New%20York&APPID=bb740b159467f35d8fe7b27bb6bcf553')
+		elif(city.city_name=="Boston"):
+			r = requests.get('http://api.openweathermap.org/data/2.5/weather?q=Boston&APPID=bb740b159467f35d8fe7b27bb6bcf553')
+		elif(city.city_name=="Philadelphia"):
+			r = requests.get('http://api.openweathermap.org/data/2.5/weather?q=Philadelphia&APPID=bb740b159467f35d8fe7b27bb6bcf553')
+		else:
+			r = requests.get('http://api.openweathermap.org/data/2.5/weather?q=Washington&APPID=bb740b159467f35d8fe7b27bb6bcf553')
+		city.temp=(r.json()['main']['temp'])
+		city.save()
 
-    return render(request, 'p2/index.html', context)
-
-
-def results(request, src_id, des_id):
-    # Apply Google Map here
-    city_src=get_object_or_404(pk=src_id)
-    city_des=get_object_or_404(pk=des_id)
-    
-    return HttpResponseRedirect('/p2')
+	# question = get_object_or_404(Question, pk=question_id)
+	src = get_object_or_404(City, pk=src_id)
+	dest = get_object_or_404(City, pk=des_id)
+	context = {'source': src, 'dest': dest}
+	return render(request, 'p2/route.html', context)
 
 
+def hp(request):
+	return render(request, 'p2/route.html')
 
-
+# def results(request, src_id=0, des_id=0):
+#     # Apply Google Map here
+#     city_src=get_object_or_404(pk=src_id)
+#     city_des=get_object_or_404(pk=des_id)
+#
+#     return HttpResponseRedirect('/p2')
+#
+#
+#
+#
+# <<<<<<< Updated upstream
+# =======
+#
+# def trip(request):
+#     if request.method == 'POST':
+#         form = Plan(request.POST)
+#         if form.is_valid():
+#             New_trip=Trip(src=form.cleaned_data['src'],des=form.cleaned_data['des'])
+#             New_trip.save()
+#             #edit to redirect to Google Map here
+#             return HttpResponseRedirect('/p2')
+#         else:
+#             form = Plan()
+#         return render(request, 'p2/plan.html', {'form': form})
+# >>>>>>> Stashed changes
