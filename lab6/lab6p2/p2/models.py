4cb0c21d57483256d81f9571015a1aee86a8cd3b
xinlijia <jiaxinli93@gmail.com>
2017-03-20 21:48:49 -0400

    Add files via upload

--- lab6/lab6p2/p2/models.py
+++ lab6/lab6p2/p2/models.py
@@ -3,6 +3,9 @@ from django.db import models
 
 class City(models.Model):
     city_name = models.CharField(max_length=200)
+    lo = models.FloatField(default=0)
+    la = models.FloatField(default=0)
+    temp = models.FloatField(default=0)
     def __str__(self):
         return self.city_name
 
--- lab6/lab6p2/p2/views.py
+++ lab6/lab6p2/p2/views.py
@@ -2,41 +2,35 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import get_object_or_404, render
 from django.core.urlresolvers import reverse
 from .forms import Plan
+import requests
 
 from .models import City, Trip
 
 def index(request):
     city_list = City.objects.order_by('city_name')[:5]
     context = {'city_list': city_list}
+    for city in city_list:
+        if(city.city_name=="New York"):
+            r = requests.get('http://api.openweathermap.org/data/2.5/weather?q=New%20York&APPID=bb740b159467f35d8fe7b27bb6bcf553')
+        elif(city.city_name=="Boston"):
+            r = requests.get('http://api.openweathermap.org/data/2.5/weather?q=Boston&APPID=bb740b159467f35d8fe7b27bb6bcf553')
+        elif(city.city_name=="Philadelphia"):
+            r = requests.get('http://api.openweathermap.org/data/2.5/weather?q=Philadelphia&APPID=bb740b159467f35d8fe7b27bb6bcf553')
+        else:
+            r = requests.get('http://api.openweathermap.org/data/2.5/weather?q=Washington&APPID=bb740b159467f35d8fe7b27bb6bcf553')
+        
+        city.temp=(r.json()['main']['temp'])
+
     return render(request, 'p2/index.html', context)
 
 
-def weather(request, city_id):
-    city = get_object_or_404(City, pk=city_id)
-    response = "Weather of City %s."
-    # Apply weather Api here
-    return render(request, 'p2/weather.html', {'city': city})
-
-def results(request, trip_id):
+def results(request, src_id, des_id):
     # Apply Google Map here
-    response = "You're looking at the results of trip %s."
-    return HttpResponse(response % trip_id)
+    city_src=get_object_or_404(pk=src_id)
+    city_des=get_object_or_404(pk=des_id)
+    
+    return HttpResponseRedirect('/p2')
 
 
-
-
-
-
-def trip(request):
-    if request.method == 'POST':
-        form = Plan(request.POST)
-        if form.is_valid():
-            New_trip=Trip(src=form.cleaned_data['src'],des=form.cleaned_data['des'])
-            New_trip.save()
-            #edit to redirect to Google Map here
-            return HttpResponseRedirect('/p2')
-        else:
-            form = Plan()
-        return render(request, 'p2/plan.html', {'form': form})
 
 
